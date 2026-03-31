ce540bbe7bfba9e43232a03e9689e6eec87ad68a
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-03-31 21:38:50 +0530

    set matrix to zero brute force approach

---
+++ src/Matrix/SetMatrixZeroes.java
@@ -0,0 +1,60 @@
+package Matrix;
+
+public class SetMatrixZeroes {
+    //Brute Force Approach
+    int n;
+    int m;
+    private void markRow(int[][] matrix,int row){
+        for(int col=0;col<m;col++){
+            if(matrix[row][col]!=0){
+                matrix[row][col]=-1;
+            }
+        }
+    }
+    private void markCol(int[][] matrix,int col){
+        for(int i=0;i<n;i++){
+            if(matrix[i][col]!=0){
+                matrix[i][col]=-1;
+            }
+        }
+    }
+    public void setZeroBrute(int[] [] matrix){
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        n = matrix.length;
+        m = matrix[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    markRow(matrix,i);
+                    markCol(matrix,j);  
+                }
+            }
+        }     
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
+                }
+                 
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+            int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+            SetMatrixZeroes smz = new SetMatrixZeroes();
+            smz.setZeroBrute(matrix);
+             for(int i=0;i<matrix.length;i++) {
+                 for (int j = 0; j < matrix[0].length; j++) {
+                     System.out.print(matrix[i][j] + " ");
+                 }
+                 System.out.println();
+             }
+    }
+}
