f0a279677d47f8c628aa2e14ac33c758af25d7d1
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-03-31 18:54:40 +0530

    find missing number using xor (optimal approach2)

--- src/Binary/FindMissingNumber.java
+++ src/Binary/FindMissingNumber.java
@@ -12,9 +12,23 @@ public class FindMissingNumber {
         return (int)(naturalSum-sum);
     }
 
+    // Using XOR: XOR of all numbers from 1 to n and XOR of all elements in the array will give the missing number.
+    int optimal2(int[] arr){
+        int xor1=0,xor2=0;
+        int n=arr.length+1;
+        for(int i=0;i<arr.length;i++){
+            xor1^=arr[i];
+        }
+        for(int i=1;i<=n;i++){
+            xor2^=i;
+        }
+        return xor1^xor2;
+    }
+
     public static void main(String[] args) {
         FindMissingNumber obj=new FindMissingNumber();
         int[] arr={1,2,3,4,6};
         System.out.println(obj.optimal1(arr));
+        System.out.println(obj.optimal2(arr));
     }
 }
