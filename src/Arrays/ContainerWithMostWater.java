fd38d5be918994d2d59ea47dd8cc95f014f0a53d
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-04-13 11:48:00 +0530

    Container with most water optimal using brute approach

--- src/Arrays/ContainerWithMostWater.java
+++ src/Arrays/ContainerWithMostWater.java
@@ -1,6 +1,21 @@
 package Arrays;
 
 public class ContainerWithMostWater {
+    //brute force approach
+    public int maxAreaBrute(int[] height){
+        int n=height.length;
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int width=j-i;
+                int minHeight=Math.min(height[i],height[j]);
+                int area=width*minHeight;
+                maxArea=Math.max(maxArea,area);
+            }
+        }
+        return maxArea;
+    }
+
     //optimal approach using two pointers
     public int maxArea(int[] height){
         int n=height.length;
@@ -21,6 +36,9 @@ public class ContainerWithMostWater {
     }
 
     public static void main(String[] args) {
-
+        ContainerWithMostWater obj=new ContainerWithMostWater();
+        int[] height={1,8,6,2,5,4,8,3,7};
+        System.out.println("Max area using brute force: "+obj.maxAreaBrute(height));
+        System.out.println("Max area using optimal approach: "+obj.maxArea(height));
     }
 }
