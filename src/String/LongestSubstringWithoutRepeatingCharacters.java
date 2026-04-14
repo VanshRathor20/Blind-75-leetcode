940c689f50c0b0c09229967e6614ae639452e1c4
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-04-15 00:07:50 +0530

    Add brute force solution for longest substring without repeating characters

---
+++ src/String/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Arrays;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int brute(String s){
+        int n=s.length();
+        int maxLen=0;
+        for(int i=0;i<n;i++){
+            int[] hash=new int[256];
+            Arrays.fill(hash,0);
+            for(int j=i;j<n;j++){
+                if(hash[s.charAt(j)]==1) break;
+                hash[s.charAt(j)]=1;
+                maxLen=Math.max(maxLen,j-i+1);
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringWithoutRepeatingCharacters obj=new LongestSubstringWithoutRepeatingCharacters();
+        String s="abcabcbb";
+        System.out.println("Length of longest substring without repeating characters: "+obj.brute(s));
+    }
+}
