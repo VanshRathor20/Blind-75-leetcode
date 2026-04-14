cf45c69811afeebb672d44fe2a3eaf6569c8e39b
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-04-15 00:34:30 +0530

    Add optimal solution for longest substring without repeating characters

--- src/String/LongestSubstringWithoutRepeatingCharacters.java
+++ src/String/LongestSubstringWithoutRepeatingCharacters.java
@@ -17,10 +17,30 @@ public class LongestSubstringWithoutRepeatingCharacters {
         }
         return maxLen;
     }
+    public int optimal(String s){
+        int n=s.length();
+        int maxLen=0;
+        int[] hash=new int[256];
+        Arrays.fill(hash,0);
+        int left=0, right=0;
+        while(right<n){
+            if((hash[s.charAt(right)])==1){
+                hash[s.charAt(left)]=0;
+                left++;
+            }
+            else{
+                hash[s.charAt(right)]=1;
+                maxLen=Math.max(maxLen,right-left+1);
+                right++;
+            }
+        }
+        return maxLen;
+    }
 
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters obj=new LongestSubstringWithoutRepeatingCharacters();
         String s="abcabcbb";
         System.out.println("Length of longest substring without repeating characters: "+obj.brute(s));
+        System.out.println("Length of longest substring without repeating characters: "+obj.optimal(s));
     }
 }
