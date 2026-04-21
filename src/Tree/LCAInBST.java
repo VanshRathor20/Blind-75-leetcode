25aedacd43d361149409da36fc2849c52342aa5f
VanshRathor20 <vanshrathor.personal@gmail.com>
2026-04-21 22:24:30 +0530

    Add implementation to find the lowest common ancestor in a binary search tree

---
+++ src/Tree/LCAInBST.java
@@ -0,0 +1,47 @@
+package Tree;
+
+public class LCAInBST {
+    /**
+     * Keep TreeNode scoped to this solution class to avoid name conflicts with
+     * other files in the same package that also declare a TreeNode type.
+     */
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        LCAInBST lcaInBST = new LCAInBST();
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+        TreeNode p = root.left; // Node with value 2
+        TreeNode q = root.right; // Node with value 8
+
+        TreeNode lca = lcaInBST.lowestCommonAncestor(root, p, q);
+        System.out.println("LCA of " + p.val + " and " + q.val + " is: " + lca.val); // Should print 6
+    }
+}
